95d91711fabb6566e2c3bab48be869a5e5fc342e
Hritik Tailor <hritiktailor@gmail.com>
2023-10-28 02:06:22 +0100

    added 3 classes

--- lib/main.dart
+++ lib/main.dart
@@ -7,3 +7,22 @@ void main() async {
    ),
  );
 }
+
+class MyFlutterApp extends StatefulWidget {
+ const MyFlutterApp({Key? key}) : super(key: key);
+
+ @override
+ State<StatefulWidget> createState() {
+   return MyFlutterState();
+ }
+}
+
+class MyFlutterState extends State<MyFlutterApp>{
+ @override
+ Widget build(BuildContext context) {
+   return const Scaffold(
+     //Flutter UI Widgets go here
+   );
+ }
+}
+
