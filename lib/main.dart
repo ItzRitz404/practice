17c9952c775561be0a13a422cea6f87eb31266e6
Hritik Tailor <hritiktailor@gmail.com>
2023-10-28 02:18:48 +0100

    added first part of scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,21 @@ class MyFlutterApp extends StatefulWidget {
 class MyFlutterState extends State<MyFlutterApp>{
  @override
  Widget build(BuildContext context) {
-   return const Scaffold(
+    return Scaffold(
      //Flutter UI Widgets go here
+      backgroundColor: Color(0xff3e87c5),
+      body: Align(
+        alignment: Alignment.center,
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            alignment: Alignment.topLeft,
+            children: [],
+          ),
+        ),
+      ),
+
    );
  }
 }
